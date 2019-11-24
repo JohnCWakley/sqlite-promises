696c3dd2adad6d3c0d48d84d1b8ea073a2db0658
John Wakley <jwakley74@gmail.com>
2019-11-23 20:16:10 -0500

    fixed verbose

--- src/logger.ts
+++ src/logger.ts
@@ -2,7 +2,9 @@ import { inspect } from 'util';
 import { gray, green, white, yellow, red } from 'chalk';
 import chalk = require('chalk');
 
-export function Logger(scope?: string, debug: boolean = false) {
+export function Logger(scope?: string, initialVerbose: boolean = false) {
+    let verbose = initialVerbose;
+
     function _log(color: Function, args: any[]) {
         args = args.map(arg => {
             return (typeof arg === 'string') ? arg : inspect(arg);
@@ -14,16 +16,27 @@ export function Logger(scope?: string, debug: boolean = false) {
         
         console.log(color(args.join(' ')));
     }
-
-    if (debug) {
-        console.log(chalk.gray((new Date()).toISOString(), 'Logger : showing debug logs'));
-    }
-
-    return {
-        debug: (...args: any) => { if (debug) _log(gray, args); },
+    
+    const methods = {
+        debug: (...args: any) => { if (verbose) _log(gray, args); },
         hero: (...args: any) => _log(green, args),
         info: (...args: any) => _log(white, args),
         warn: (...args: any) => _log(yellow, args),
-        error: (...args: any) => _log(red, args)
+        error: (...args: any) => _log(red, args),
+        verbose: (_verbose: boolean = true) => {
+            if (_verbose) {
+                verbose = _verbose;
+                methods.debug('Logger: verbose: true');
+            } else {
+                methods.debug('Logger: verbose: false');
+                verbose = _verbose;
+            }
+        }
     };
+
+    if (verbose) {
+        methods.debug('Logger: verbose: true');
+    }
+
+    return methods;
 }
